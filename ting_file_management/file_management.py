21217b865a460dc86b1f68b3bfba22bbad493d9d
Adriano Monteiro <adrianomonteiroweb@mail.com>
2022-05-14 14:26:11 -0300

    NEW: req 1

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+
+def txt_importer(path_file: str):
+    if (path_file.endswith(".txt") is False):
+        sys.stderr.write('Formato inválido\n')
+        return
+
+    try:
+        with open(path_file) as file:
+            return [f.rstrip() for f in file.readlines()]
+
+    except FileNotFoundError:
+        sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
