08e48c3885091dad46464878ddaa65b3dc6e3546
Adriano Monteiro <adrianomonteiroweb@mail.com>
2022-05-16 12:14:48 -0300

    NEW: req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,24 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+import sys
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+
+
+def process(path_file: str, instance: Queue):
+    object_file = {}
+
+    object_file["nome_do_arquivo"] = path_file
+    object_file["qtd_linhas"] = len(txt_importer(path_file))
+    object_file["linhas_do_arquivo"] = txt_importer(path_file)
+
+    print(object_file, file=sys.stdout)
+
+    for file in range(len(instance)):
+        if (instance.search(file) == object_file):
+            return object_file
+
+    instance.enqueue(object_file)
+
+    return object_file
 
 
 def remove(instance):
