346dc84870aadde8173d8d17acc94fa5f4785c19
Adriano Monteiro <adrianomonteiroweb@mail.com>
2022-05-16 14:19:59 -0300

    NEW: 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,12 +1,13 @@
+import re
 from ting_file_management.queue import Queue
 
 
 def exists_word(word, instance: Queue):
     line = []
 
-    for index, row in enumerate(instance.search(0)["linhas_do_arquivo"]):
+    for content, row in enumerate(instance.search(0)["linhas_do_arquivo"]):
         if word.lower() in row.lower():
-            line.append({"linha": index + 1})
+            line.append({"linha": content + 1})
 
     if len(line) == 0:
         return []
@@ -19,4 +20,18 @@ def exists_word(word, instance: Queue):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    ocurrence, counter = [], 1
+
+    for content in instance.search(0)['linhas_do_arquivo']:
+        if re.search(word, content, re.IGNORECASE):
+            ocurrence.append({'linha': counter, 'conteudo': content})
+
+        counter += 1
+
+    file = instance.search(0)['nome_do_arquivo']
+
+    message = [{'palavra': word, 'arquivo': file, 'ocorrencias': ocurrence}]
+
+    return [] if len(ocurrence) == 0 else message
+
+# https://stackoverflow.com/questions/500864/case-insensitive-regular-expression-without-re-compile
